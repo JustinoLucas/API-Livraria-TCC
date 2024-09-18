8b2158f990d47f6e1e445464c1efb8f170ab4638
Lucas Justino <98190570+JustinoLucas@users.noreply.github.com>
2024-09-18 11:35:25 -0300

    Update conexao.php

--- config/conexao.php
+++ config/conexao.php
@@ -1,17 +1,6 @@
 <?php 
 
 // ** MySQL Settings ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'nomedb');
-
-/** MySQL database username */
-define('DB_USER', 'usernamedb');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'passdb');
-
-/** MySQL hostname */
-define('DB_HOST', 'ipdb');
 
 $hostname = "localhost";
 $bancodedados = "estoque_livro";
